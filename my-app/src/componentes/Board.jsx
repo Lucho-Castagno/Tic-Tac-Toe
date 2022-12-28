8aa711eb09446cc2922c5dcdab4223ede6c98ed9
unknown <lucho27ris@gmail.com>
2022-12-28 20:47:10 -0300

    nuevos cambios en Board y Square

--- my-app/src/componentes/Board.jsx
+++ my-app/src/componentes/Board.jsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Square from './Square';
 
 const Board = () => {
+
+    // el estado del tablero, donde squares es el tablero con los 9 cuadrados (de 0 a 8)
+    // donde cada cuadrado tendra al comienzo el valor 'null' y luego, dependiendo del jugador
+    // tendra el valor 'X' o 'O' quedando, por ejemplo, una matriz con los siguientes valores:
+    // [
+    // 'O', null, 'X',
+    // 'X', 'X', 'O',
+    // 'O', null, null,
+    // ]
+    // useState para cambiar el "estado"/valor de una variable (squares) con una funcion (setSquares),
+    // asignada en un arreglo usando useState con un arreglo de 9 posiciones con nulls como valor por defecto de squares.
+    const [squares, setSquares] = useState(Array(9).fill(null));
+
+    const handleClick = (i) => {
+        const sq = squares.slice();
+        sq[i] = 'X';
+        setSquares(sq);
+    };
+
+    // para saber el estado de los cuadrados, mandamos una funcion en el onClick para que el 
+    // cuadrado pueda actualizar el estado de su padre (Board).
     const renderSquare = (i) => {
-        return <Square value={i} />
-    }
+        return <Square value={squares[i]} onClick={() => handleClick(i)} />
+    };
 
     const status = 'Siguiente jugador: X';
 
--- my-app/src/componentes/Square.jsx
+++ my-app/src/componentes/Square.jsx
@@ -1,14 +1,9 @@
-import React, {useState} from 'react';
+import React from 'react';
 
 const Square = (props) => {
-
-    // useState para cambiar el "estado"/valor de una variable (value) con una funcion (setValue),
-    // asignada en un arreglo usando useState con null como valor por defecto de value.
-    const [value, setValue] = useState(null);
-
     return (
-        <button className='square' onClick={() => setValue('X') }>
-            {value}
+        <button className='square' onClick={props.onClick}>
+            {props.value}
         </button>
     )
 };
